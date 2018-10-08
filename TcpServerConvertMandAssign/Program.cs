001c93379a3c5c8f1000fdb326507f5bb6ed36a4
Mario Andres Ravina <ayarmarxio@gmail.com>
2018-10-08 09:32:07 +0200

    Add project files.

---
+++ TcpServerConvertMandAssign/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TcpServerConvertMandAssign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
